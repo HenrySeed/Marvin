ff61aab80fec55e58ad003c788a34b34cc9b916c
Henry Seed <henryseed@Henrys-Liquor.local>
2016-10-03 16:34:32 +1300

    Fixed calculator

--- calc/calc.py
+++ calc/calc.py
@@ -1,12 +1,15 @@
 def calculator():
     '''type calculator and then you can enter equations to be answered'''
     done = False
+    print('CALC: (c) Henry Seed 2016 \n\
+      Enter an equation or "q" to quit:')
     while done == False:
-        equation = input('CALC: (c) Henry Seed 2016 \n\
-        Enter an equation or "q" to quit:\n\n    > ')
+        equation = input('\nCALC: > ')
 
-        try:
-            print('\n    = ' + str(eval(equation)))
+        if equation == 'q':
             done = True
-        except:
-            print('\n    Please enter a valid equation or "q" to quit.\n')
+        else:
+            try:
+                print('      = ' + str(eval(equation)))
+            except:
+                print('\n      Please enter a valid equation or "q" to quit.')
